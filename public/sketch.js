1c15dc0a720bf2215baf56d989ca7be18414130c
peb <robin.ansias@gmail.com>
2021-10-21 23:11:40 +0200

    added leg1

--- public/sketch.js
+++ public/sketch.js
@@ -93,6 +93,7 @@ function preload() {
   spriteSoccerBall = loadImage('../assets/sprite_soccer_ball.png');
   spritePlayerMainBody0 = loadImage('../assets/sprite_player_main_body0.png');
   spritePlayerLeg0 = loadImage('../assets/sprite_player_leg0.png');
+  spritePlayerLeg1 = loadImage('../assets/sprite_player_leg1.png');
 }
 
 // Entry point of code
